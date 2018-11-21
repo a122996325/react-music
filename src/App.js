a8aec18b0c92e434210c06e2deb14dc8c4edd116
郭振山 <guozhenshan@chengla.net>
2018-11-21 13:21:45 +0800

    备份

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Button } from 'antd-mobile';
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Button className="btn">hello world</Button>
+        hello
       </div>
     );
   }
---
+++ src/components/PlayContainer.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import './PlayContaner.css';
+class PlayContaner extends Component {
+    render () {
+
+    }
+}
+export default PlayContaner
