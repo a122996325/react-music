691c584a1d3ed1139a3ca3143ccd97e39742380a
郭振山 <guozhenshan@chengla.net>
2018-11-21 12:56:34 +0800

    更新

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+import { Button } from 'antd-mobile';
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Button className="btn">hello world</Button>
+      </div>
+    );
+  }
+}
+
+export default App;
