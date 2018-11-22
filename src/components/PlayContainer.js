eeffc01eabe9608c949b317c4a842c0e95eef8b0
郭振山 <guozhenshan@chengla.net>
2018-11-22 16:59:15 +0800

    更新

--- src/components/PlayContainer.js
+++ src/components/PlayContainer.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
-import './PlayContaner.css';
+import '../assets/css/PlayContainer';
 class PlayContaner extends Component {
+    constructor (props){
+        super(props)
+    }
     render () {
-
+        return (
+            <div>
+                
+            </div>
+        )
     }
 }
 export default PlayContaner
