8420b2549feaf63225cb235fd5ab959c0c92f126
a122996325 <a122996325@gmail.com>
2018-11-26 02:35:45 +0800

    重置项目

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-import PlayContainer from "./components/PlayContainer";
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <PlayContainer></PlayContainer>
+        hello world
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+// 引入全局样式
 import './assets/css/index.css'
 ReactDOM.render(<App />, document.getElementById('root'));
 
